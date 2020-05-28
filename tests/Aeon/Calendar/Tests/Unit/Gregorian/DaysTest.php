ccf7abb5fb6b1dbdf8515cc30bebd47447349247
Norbert Orzechowicz <norbert@orzechowicz.pl>
2020-05-28 21:12:57 +0200

    Configured php cs fixer and added it to composer test script

--- tests/Aeon/Calendar/Tests/Unit/Gregorian/DaysTest.php
+++ tests/Aeon/Calendar/Tests/Unit/Gregorian/DaysTest.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the Aeon time management framework for PHP.
- *
- * (c) Norbert Orzechowicz <contact@norbert.tech>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Aeon\Calendar\Tests\Unit\Gregorian;
 
 use Aeon\Calendar\Gregorian\Day;
