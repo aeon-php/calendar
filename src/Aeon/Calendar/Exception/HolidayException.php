53feef44b81f97ebe466a07c8188a300dbcb70b3
Norbert Orzechowicz <norbert@orzechowicz.pl>
2020-06-19 20:35:05 +0200

    Removed useless interfaces

--- src/Aeon/Calendar/Exception/HolidayException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Aeon\Calendar\Exception;
-
-class HolidayException extends Exception
-{
-}
--- src/Aeon/Calendar/Exception/HolidayYearException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Aeon\Calendar\Exception;
-
-final class HolidayYearException extends HolidayException
-{
-}
